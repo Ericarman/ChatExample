98adcd682055734b062e0e43538ba7d35807ab87
Eric Hovhannisyan <Erichovhannisyan13@gmail.com>
2024-12-10 16:27:41 +0400

    Initial setup

---
+++ HealthChatExample/HealthChatExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthChatExampleApp.swift
+//  HealthChatExample
+//
+//  Created by Eric Hovhannisyan on 10.12.24.
+//
+
+import SwiftUI
+
+@main
+struct HealthChatExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
